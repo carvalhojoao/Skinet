7492a68d502a958e5cf553f9cfe4c3da02e2512c
João Victor <joaov_rc@outlook.com>
2025-02-07 01:08:43 -0300

    Specification

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -15,7 +15,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
         {
-            var spec = new ProductSpecification(brand, type);
+            var spec = new ProductSpecification(brand, type, sort);
             var products = await repo.ListAsync(spec);
             return Ok(products);
         }
@@ -78,15 +78,15 @@ namespace API.Controllers
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
         {
-            //TODO: Implement method
-            return Ok();
+            var spec = new BrandListSpecification();
+            return Ok(await repo.ListAsync(spec));
         }
 
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
         {
-            //TODO: Implement method
-            return Ok();
+            var spec = new TypeListSpecification();
+            return Ok(await repo.ListAsync(spec));
         }
 
         private bool ProductExists(int id)
