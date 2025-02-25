3e7b47e94b3d2b4f7a5732745a7047e24f7a6bd2
João Victor <joaov_rc@outlook.com>
2025-02-25 00:02:43 -0300

    http request

--- API/Program.cs
+++ API/Program.cs
@@ -20,7 +20,8 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+    .WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
